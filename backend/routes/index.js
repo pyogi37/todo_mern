8633f57f975710b8cda0651200a95547b1cefc20
pyogi37 <pyogi37@gmail.com>
2023-11-04 10:51:37 +0530

    optimized routing

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,8 +5,8 @@ const router = express.Router();
 console.log("Router loaded");
 router.use("/api", require("./api"));
 
-router.get("/", async (req, res) => {
-  await res.json({ message: "API is running" });
-});
+// router.get("/", async (req, res) => {
+//   await res.json({ message: "API is running, go to /home" });
+// });
 
 module.exports = router;
